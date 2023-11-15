99403e61ab6c2d685bf613221f2a4cf4ad1892f6
김보경 <love2491193@naver.com>
2023-11-15 17:47:02 +0900

    board file upload

--- src/views/board/upload.vue
+++ src/views/board/upload.vue
@@ -7,70 +7,209 @@
         </div>
         <div class="content-input-container">
           <label for="content">Content:</label>
-          <input type="text" id="content" v-model="content" class="content-input" />
+          <textarea type="text" id="content" v-model="content" class="content-input"></textarea>
         </div>
-        <div class="location-input-container">
-          <label for="location">Location:</label>
-          <input type="text" id="location" v-model="location" class="location-input" />
+        <div >
+          <div class="location-input-container">
+            <label for="location">Location:</label>
+            <input type="text" id="location" v-model="searchQuery" @keyup.enter="searchPlaces" class="location-input" />
+            <button class="circle-button" @click="searchPlaces">Search</button>
+          </div>
+          <hr>
+            <div id="map" style="width:100%; height:400px; background-color: transparent;"></div>
+            <div id="clickLatlng"></div>
         </div>
-        <input type="file" @change="handleFiles" multiple accept="image/*" class="file-input" />
+        <div class="file-input-container">
+          <input type="file" id="file" @change="handleFiles" multiple accept="image/*" class="file-input" />
+          <label for="file" class="file-label">이미지 선택</label>
+        </div>
+        
     
-        <button @click="uploadFiles" class="upload-btn">Upload</button>
       </div>
       <ul v-if="files.length" class="file-list">
-      <li v-for="file in files" :key="file.name" class="file-item">{{ file.name }}</li>
-    </ul>
+        <li v-for="file in files" :key="file.name" class="file-item">{{ file.name }}</li>
+      </ul>
+      <button @click="uploadFiles" class="upload-btn">Upload</button>
     </div>
-  </template>
+  </template>location
   
-  <script setup>
-import { ref } from 'vue';
-import axios from 'axios';
+<script setup>
+  import { onMounted, ref } from 'vue';
+  import axios from 'axios';
 
-const files = ref([]);
-const title = ref('');
+  const files = ref([]);
+  const title = ref('');
 
-const handleFiles = (event) => {
-  files.value = Array.from(event.target.files);
-};
+  const searchQuery = ref('');
+  const mapContainer = ref(null);
+  const map = ref(null);
+  const infowindow = ref(null);
+  const markers = ref([]);
+  const inLocation = ref({});
+  
 
-const uploadFiles = async () => {
-  const formData = new FormData();
-  formData.append('title', title.value);
-  files.value.forEach(file => {
-    formData.append('files', file);
-  });
-
-  try {
-    const response = await axios.post('YOUR_API_ENDPOINT', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
+  const handleFiles = (event) => {
+    files.value = Array.from(event.target.files);
+    if (files.value.length > 10) {
+        alert('최대 10개의 파일만 업로드할 수 있습니다.');
+        event.target.value = ''; // 입력 필드 초기화
+      } else {
+        files.value = selectedFiles;
       }
+  };
+
+  const uploadFiles = async () => {
+    console.log(title.value)
+    console.log(content.value)
+    console.log(inLocation.value)
+
+    const formData = new FormData();
+    formData.append('title', title.value);
+    formData.append('content',content.value)
+    formData.append('inLocation', JSON.stringify(inLocation.value))
+    files.value.forEach(file => {
+      formData.append('files', file);
     });
-    console.log('File uploaded successfully', response.data);
-  } catch (error) {
-    console.error('Error uploading file', error);
+
+    try {
+      const response = await axios.post('/board', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      console.log('File uploaded successfully', response.data);
+    } catch (error) {
+      console.error('Error uploading file', error);
+    }
+  };
+
+
+  const searchPlaces = () => {
+    console.log(searchQuery)
+    if (searchQuery.value) {
+
+      const ps = new kakao.maps.services.Places();
+      ps.keywordSearch(searchQuery.value, placesSearchCB);
+      kakao.maps.load();
+    }
+  };
+  const loadMap = () => {
+    // 카카오 맵 라이브러리 로딩
+    const mapOption = {
+      center: new kakao.maps.LatLng(37.566826, 126.9786567),
+      level: 3
+    };
+
+    mapContainer.value = document.getElementById('map')
+    const iwContent = '<div style="padding:5px;">Hello World!</div>';
+    const iwRemoveable = true;
+    infowindow.value = new kakao.maps.InfoWindow({
+      content: iwContent,
+      removable: iwRemoveable
+    });
+
+    map.value = new kakao.maps.Map(mapContainer.value, mapOption);
+    searchPlaces();
+    // const ps = new kakao.maps.services.Places();
+    // ps.keywordSearch('이성당', placesSearchCB);
+
   }
-};
+  const loadLocation = () => {
+    const script = document.createElement("script")
+    script.type = "text/javascript"
+    script.src = import.meta.env.VITE_KAKAOLOCATIONS
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      kakao.maps.onloadcallbacks.push(loadMap);
+      kakao.maps.load();
+    }
+
+  };
+  const placesSearchCB = (data, status, pagination) => {
+    if (status === kakao.maps.services.Status.OK) {
+      clearMarkers();
+      var bounds = new kakao.maps.LatLngBounds();
+      for (let i = 0; i < data.length; i++) {
+        displayMarker(data[i]);
+        bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+      }
+      map.value.setBounds(bounds);
+    }
+  };
+  const displayMarker = (place) => {
+    // clearMarkers()
+    var marker = new kakao.maps.Marker({
+      map: map.value,
+      position: new kakao.maps.LatLng(place.y, place.x)
+    });
+    markers.value.push(marker); // Add the new marker to the array
+
+    kakao.maps.event.addListener(marker, 'click', () => {
+      infowindow.value.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+      infowindow.value.open(map.value, marker);
+
+      var message = place.place_name+'/' + place.road_address_name + " /" + place.category_group_name;
+      // place.x place.y
+      // console.log(infowindow.value);
+      inLocation.value = {
+        locationName : place.place_name,
+        locationAddr : place.road_address_name,
+        locationLat : place.x,
+        locationLon : place.y,
+        locationType : place.category_group_name
+      };
+      var resultDiv = document.getElementById('clickLatlng');
+      resultDiv.innerHTML = message;
+    });
+  };
+
+  const clearMarkers = () => {
+    markers.value.forEach(marker => {
+      marker.setMap(null); // Remove marker from the map
+    });
+    markers.value = []; // Clear the markers array
+  };
+
+  onMounted(loadLocation);
+
 </script>
   <style>
+   label {
+    font-size: 30px; /* 글자 크기 증가 */
+  }
+  label[for="file"] {
+    font-size: 20px; /* 글자 크기 증가 */
+  }
   /* 전체 컨테이너를 중앙 정렬 */
   .upload-container {
+    padding: 30px; 
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     margin: 0 auto;
     padding: 20px;
-    max-width: 500px; /* 적당한 최대 너비 */
     border: 1px solid #ddd;
     border-radius: 8px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 카드 스타일의 그림자 */
+    width: 50%; /* 창의 50% 너비 */
+    min-width: 500px; /* 최소 너비 설정 */
+    max-width: 80%; /* 최대 너비를 80%로 설정하여 유연성 부여 */
+    margin: 0 auto; /* 좌우 자동 마진으로 중앙 정렬 */
+    padding: 20px;
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+
+    position: relative; /* 포지션 설정 */
+    /* top: 100%; 상단에서 50% 위치 */
+    transform: translateY(40%); /* Y축 기준 50% 위로 이동 */
   }
   .upload-btn {
   cursor: pointer;
   padding: 10px 20px;
-  background-color: #007BFF; /* 밝은 파란색 */
+  background-color: #a9bbce; /* 밝은 파란색 */
   color: white;
   border: none;
   border-radius: 5px; /* 둥근 모서리 */
@@ -81,23 +220,25 @@ const uploadFiles = async () => {
 }
 
 .upload-btn:hover, .upload-btn:focus {
-  background-color: #0056b3; /* 어두운 파란색으로 변경 */
+  background-color: #82a7cf; /* 어두운 파란색으로 변경 */
   transform: scale(1.05); /* 버튼을 약간 확대 */
   outline: none; /* 포커스시 윤곽선 제거 */
 }
   
-  .input-container {
+  input,textarea {
+    padding: 12px; /* 패딩 증가 */
+    font-size: 30px; /* 글자 크기 증가 */
     width: 100%;
   }
-  
   /* 레이블과 인풋 필드 스타일링 */
   .title-input-container,
   .content-input-container,
   .location-input-container {
     margin-bottom: 10px;
     display: flex;
-    flex-direction: column;
-    align-items: start;
+    
+    align-items: center;
+    gap: 25px;
   }
   
   .title-input,
@@ -108,6 +249,7 @@ const uploadFiles = async () => {
     border: 1px solid #ddd;
     border-radius: 4px;
     margin-top: 5px;
+    
   }
   
   .file-input {
@@ -123,6 +265,60 @@ const uploadFiles = async () => {
     padding: 8px;
     border: 1px solid #ddd;
     border-radius: 4px;
+  }
+  .file-input-container {
+    position: relative;
+    margin-bottom: 10px;
+  }
+
+  .file-input {
+    width: 100%;
+    height: 40px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    opacity: 0;
+    cursor: pointer;
+  }
+
+  .file-label {
+    display: block;
+    background-color: #a9bbce;
+    color: white;
+    text-align: center;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+
+  .file-label:hover {
+    background-color: #a9bbce;
+  }
+  .circle-button {
+    display: flex;
+    padding: 0 20px;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5px;
+    width: 80px; /* 원의 지름 */
+    height: 40px; /* 원의 지름 */
+    line-height: 40px;
+    border-radius: 5px; /* 원형으로 만듦 */
+    border: none; /* 테두리 없앰 */
+    background-color: #a9bbce; /* 배경 색상 */
+    color: white; /* 글자 색상 */
+    font-size: 24px; /* 글자 크기 */
+    cursor: pointer; /* 커서 스타일 변경 */
+    outline: none; /* 포커스시 윤곽선 제거 */
+    transition: background-color 0.3s; /* 배경색 변경시 부드러운 전환 효과 */
+  }
+
+  .circle-button:hover {
+    background-color: #a9bbce; /* 호버시 배경 색상 변경 */
+  }
+  #map{
+     background-color: transparent !important;
   }
   </style>
   
